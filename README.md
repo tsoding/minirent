8c7af0322cbf26d36914534821551b4d32fcf0e1
rexim <reximkut@gmail.com>
2021-01-27 02:17:00 +0700

    Ready. Set. Go!

---
+++ README.md
@@ -0,0 +1,42 @@
+# minirent
+
+A subset of [dirent](https://pubs.opengroup.org/onlinepubs/9699919799/) interface for Windows.
+
+The code that works with minirent must work with the dirent.
+
+## Usage
+
+[minirent.h](./minirent.h) is an [stb-style](https://github.com/nothings/stb/blob/master/docs/stb_howto.txt) header-only library. That means that when you just include it it does not include the implementations of the functions. You have to define `MINIRENT_IMPLEMENTATION` macro:
+
+```c
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+#    define MINIRENT_IMPLEMENTATION
+#    include <minirent.h>
+#else
+#    include <dirent.h>
+#endif // _WIN32
+
+int main(void)
+{
+    DIR *dir = opendir(".");
+    assert(dir);
+
+    errno = 0;
+    struct dirent *dp = NULL;
+    while (dp = readdir(dir)) {
+        printf("%s\n", dp->d_name);
+    }
+    assert(errno == 0);
+
+    int err = closedir(dir);
+    assert(err == 0);
+
+    return 0;
+}
+```
+
+For more information see [./examples/](./examples/) folder.
---
+++ examples/ls.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#ifdef _WIN32
+#    define MINIRENT_IMPLEMENTATION
+#    include <minirent.h>
+#else
+#    include <dirent.h>
+#endif // _WIN32
+
+int main(int argc, char *argv[])
+{
+    const char *dir_path = ".";
+
+    if (argc >= 2) {
+        dir_path = argv[1];
+    }
+
+    DIR *dir = opendir(dir_path);
+    if (dir == NULL) {
+        fprintf(stderr, "ERROR: could not open directory %s: %s\n",
+                dir_path, strerror(errno));
+        exit(1);
+    }
+
+    errno = 0;
+    struct dirent *dp = NULL;
+    while (dp = readdir(dir)) {
+        // TODO: the output of ls is not sorted
+        printf("%s\n", dp->d_name);
+    }
+
+    if (errno) {
+        fprintf(stderr, "ERROR: could not read directory %s: %s\n",
+                dir_path, strerror(errno));
+        exit(1);
+    }
+
+    if (closedir(dir) < 0) {
+        fprintf(stderr, "ERROR: could not close directory %s: %s\n",
+                dir_path, strerror(errno));
+        exit(1);
+    }
+
+    return 0;
+}
